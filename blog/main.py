3a2ebf38ac7c46ebb01960b4a6ad42085745fb12
ValDev <val.aroztica@gmail.com>
2023-10-05 21:20:28 -0300

    update

--- blog/main.py
+++ blog/main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI, Depends, status, Response, HTTPException  # Correct
 from . import models, schemas
 from .database import engine, SessionLocal
 from sqlalchemy.orm import Session 
-
+from typing import List
 
 app = FastAPI()
 
@@ -26,17 +26,36 @@ def create(request: schemas.Blog, db : Session = Depends(get_db)):
 
 @app.delete('/blog/{id}', status_code=status.HTTP_204_NO_CONTENT)
 def destroy(id, db: Session = Depends(get_db)):
-    db.query(models.Blog).filter(models.Blog.id == id).delete(synchronize_session=False)
+    blog = db.query(models.Blog).filter(models.Blog.id == id)
+    if not blog.first():
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, 
+                            detail=f"Blog with id {id} not found")
+    
+    
+    blog.delete(synchronize_session=False)
 
     db.commit()
     return 'done'
 
-@app.get('/blog')
+
+@app.put('/blog/{id}', status_code=status.HTTP_202_ACCEPTED)
+def update(id, request: schemas.Blog, db : Session = Depends(get_db)):
+    blog = db.query(models.Blog).filter(models.Blog.id == id)
+    if not blog.first():
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, 
+                            detail=f"Blog with id {id} not found")
+    
+    update_data = request.dict()
+    blog.update(update_data)
+    db.commit()
+    return 'updated'
+
+@app.get('/blog', response_model=List[schemas.ShowBlog])
 def all(db : Session = Depends(get_db)):
     blogs = db.query(models.Blog).all()
     return blogs
 
-@app.get('/blog/{id}', status_code=200)
+@app.get('/blog/{id}', status_code=200, response_model=schemas.ShowBlog)
 def show(id, response: Response, db : Session = Depends(get_db)):
     blog = db.query(models.Blog).filter(models.Blog.id == id).first()
     if not blog:
@@ -45,6 +64,17 @@ def show(id, response: Response, db : Session = Depends(get_db)):
        #response.status_code = status.HTTP_404_NOT_FOUND
        #return {'detail': f"Blog with the id {id} is not available"}
     return blog 
+
+
+
+@app.post('/user')
+def create_user(request: schemas.User,db: Session = Depends(get_db)):
+    new_user = models.User(name=request.name,email=request.email,password=request.password)
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+
+    return new_user
 
 
 
